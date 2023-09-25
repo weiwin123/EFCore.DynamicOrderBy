d644e2856356568a9fa75027c4c927047139ce3a
chengyanwei <594001918@qq.com>
2023-09-25 16:59:06 +0800

    init

---
+++ EFCore.DymicOrderBy/QueryOrderByExtend.cs
@@ -0,0 +1,110 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace EFCore.DynamicOrderBy
+{
+    public static class QueryOrderByExtend
+    {
+
+        public static IQueryable<T> DynamicOrderBy<T>(this IQueryable<T> query, List<DynamicOrderByPredicate> dymicOrderByPredicates)
+        {
+            if (dymicOrderByPredicates.Count == 0)
+            {
+                throw new ArgumentException($"DynamicOrderBy：必须有一个排序条件");
+            }
+            var i = 0;
+
+            foreach (var d in dymicOrderByPredicates)
+            {
+                ParameterExpression p = Expression.Parameter(typeof(T));
+                Expression key = Expression.Property(p, d.FieldName);
+                var propInfo = GetPropertyInfo(typeof(T), d.FieldName);
+                var expr = GetOrderExpression(typeof(T), propInfo);
+
+
+                if (!string.IsNullOrWhiteSpace(d.FieldName))
+                {
+                    if (i == 0)
+                    {
+                        if (d.IsDesc)
+                        {
+                            var method = typeof(Queryable).GetMethods().FirstOrDefault(m => m.Name == "OrderByDescending" && m.GetParameters().Length == 2);
+                            var genericMethod = method.MakeGenericMethod(typeof(T), propInfo.PropertyType);
+                            query = (IQueryable<T>)genericMethod.Invoke(null, new object[] { query, expr });
+                        }
+                        else
+                        {
+                            var method = typeof(Queryable).GetMethods().FirstOrDefault(m => m.Name == "OrderBy" && m.GetParameters().Length == 2);
+                            var genericMethod = method.MakeGenericMethod(typeof(T), propInfo.PropertyType);
+                            query = (IQueryable<T>)genericMethod.Invoke(null, new object[] { query, expr });
+                        }
+
+                    }
+                    else
+                    {
+                        if (d.IsDesc)
+                        {
+                            var method = typeof(Queryable).GetMethods().FirstOrDefault(m => m.Name == "ThenByDescending" && m.GetParameters().Length == 2);
+                            var genericMethod = method.MakeGenericMethod(typeof(T), propInfo.PropertyType);
+                            query = (IQueryable<T>)genericMethod.Invoke(null, new object[] { query, expr });
+                        }
+                        else
+                        {
+                            var method = typeof(Queryable).GetMethods().FirstOrDefault(m => m.Name == "ThenBy" && m.GetParameters().Length == 2);
+                            var genericMethod = method.MakeGenericMethod(typeof(T), propInfo.PropertyType);
+                            query = (IQueryable<T>)genericMethod.Invoke(null, new object[] { query, expr });
+                        }
+
+                    }
+                }
+
+                i++;
+
+            }
+
+            return query;
+        }
+        /// <summary>
+        /// 获取model属性
+        /// </summary>
+        /// <param name="objType"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        private static PropertyInfo GetPropertyInfo(Type objType, string name)
+        {
+            var properties = objType.GetProperties();
+            var matchedProperty = properties.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (matchedProperty == null)
+                throw new ArgumentException($"DynamicOrderBy：没找到{name}字段");
+
+            return matchedProperty;
+        }
+        /// <summary>
+        /// 生成表达式
+        /// </summary>
+        /// <param name="objType"></param>
+        /// <param name="pi"></param>
+        /// <returns></returns>
+        private static LambdaExpression GetOrderExpression(Type objType, PropertyInfo pi)
+        {
+            var paramExpr = Expression.Parameter(objType);
+            var propAccess = Expression.PropertyOrField(paramExpr, pi.Name);
+            var expr = Expression.Lambda(propAccess, paramExpr);
+            return expr;
+        }
+    }
+
+    public class DynamicOrderByPredicate
+    {
+        /// <summary>
+        /// 字段名
+        /// </summary>
+        public string FieldName { get; set; }
+        /// <summary>
+        /// 是否倒序
+        /// </summary>
+        public bool IsDesc { get; set; }
+    }
+}
---
+++ EFCore.DynamicOrderBy.Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using EFCore.DynamicOrderBy.Demo.Models;
+using EFCore.DynamicOrderBy;
+namespace EFCore.DynamicOrderBy.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using testOrderByContext ctx = new testOrderByContext();
+            //List<TTest> list = new List<TTest>(); 
+            //for(var i = 0; i < 50; i++ )
+            //{
+            //    TTest t = new TTest();
+            //    t.SortIndex = i;
+            //    t.CreateTime = DateTime.Now;
+            //    if(i%5==0)
+            //    {
+            //        t.Abc = "a";
+            //    }
+            //    else if (i % 7 == 0){
+            //        t.Abc = "b";
+            //    }
+            //    else
+            //    {
+            //        t.Abc = "c";
+            //    }
+            //    list.Add(t);
+
+            //}
+            //ctx.TTest.AddRange(list);
+            //ctx.SaveChanges();
+            var querys= ctx.TTest.Where(t=>t.Abc=="a"||t.Abc=="b").DynamicOrderBy(new List<DynamicOrderByPredicate>() { 
+            new DynamicOrderByPredicate() {FieldName="SortIndex",IsDesc=true},
+              new DynamicOrderByPredicate() {FieldName="CreateTime",IsDesc=true},
+            }).ToList();
+        }
+    }
+}
